1b16b0c5ee9be05220659a5276bd8d7820aab884
Oleg Puzanov <puzanov@gmail.com>
2018-08-25 17:01:00 +0600

    https://stackoverflow.com/questions/25192794/no-module-named-pip-req

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@
 """The setup script."""
 import os
 
-from pip._internal.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 from setuptools import find_packages, setup
 
 with open('README.rst') as readme_file:
